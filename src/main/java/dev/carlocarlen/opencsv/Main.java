9ba0ee573febc34bc3217776e672f008fb367fce
Carlo <60157120+carlocarlen@users.noreply.github.com>
2024-09-10 21:23:45 +0200

    OpenCsv - Main

---
+++ src/main/java/dev/carlocarlen/opencsv/Main.java
@@ -0,0 +1,35 @@
+package dev.carlocarlen.opencsv;
+
+import dev.carlocarlen.opencsv.model.Person;
+import dev.carlocarlen.opencsv.model.PersonFactory;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        List<Person> persons = createPersons();
+        Writer fileWriter = new FileWriter("persons.csv");
+
+
+    }
+
+    private static List<Person> createPersons() {
+        return List.of(
+                PersonFactory.createPerson("John", "Doe", getBirthdate(1980, 5, 15), "123 Main St", 98101, "Seattle"),
+                PersonFactory.createPerson("Jane", "Smith", getBirthdate(1995, 9, 20), "456 Elm Ave", 90001, "Los Angeles")
+        );
+    }
+
+    private static Date getBirthdate(int year, int month, int day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, day);
+        return calendar.getTime();
+    }
+
+}
