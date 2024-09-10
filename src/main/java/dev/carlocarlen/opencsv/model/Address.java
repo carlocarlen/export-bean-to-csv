3f782f8ad609e0e3c4faf0ae563a9db5f68a579c
Carlo <60157120+carlocarlen@users.noreply.github.com>
2024-09-10 21:08:35 +0200

    OpenCsv - Model

---
+++ src/main/java/dev/carlocarlen/opencsv/model/Address.java
@@ -0,0 +1,31 @@
+package dev.carlocarlen.opencsv.model;
+
+public class Address {
+    private String street;
+    private Integer zip;
+    private String city;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Integer getZip() {
+        return zip;
+    }
+
+    public void setZip(Integer zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
---
+++ src/main/java/dev/carlocarlen/opencsv/model/PersonFactory.java
@@ -0,0 +1,27 @@
+package dev.carlocarlen.opencsv.model;
+
+import java.util.Date;
+
+public class PersonFactory {
+    private PersonFactory() {}
+
+    public static Person createPerson(String firstName,
+                                      String lastName,
+                                      Date birthdate,
+                                      String street,
+                                      Integer zip,
+                                      String city) {
+
+        Person person = new Person();
+        Address address = new Address();
+        address.setCity(city);
+        address.setStreet(street);
+        address.setZip(zip);
+        person.setAddress(address);
+        person.setLastName(lastName);
+        person.setFirstName(firstName);
+        person.setBirthdate(birthdate);
+
+        return person;
+    }
+}
